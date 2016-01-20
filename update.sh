d9c0b9adc63fdb03d2cb11a05bfdf2547089e476
tailot <tailot@gmail.com>
2016-01-20 10:09:35 +0100

    Update hosts

--- update.sh
+++ update.sh
@@ -1,8 +1,14 @@
-wget http://i2p-projekt.i2p/hosts.txt -e use_proxy=yes -e http_proxy=127.0.0.1:4444 -O i2p-projekt.i2p/hosts || git checkout i2p-projekt.i2p/hosts
-wget http://i2pjump.i2p/hosts -e use_proxy=yes -e http_proxy=127.0.0.1:4444 -O i2pjump.i2p/hosts || git checkout i2pjump.i2p/hosts
-wget http://inr.i2p/export/alive-hosts.txt -e use_proxy=yes -e http_proxy=127.0.0.1:4444 -O inr.i2p/hosts || git checkout inr.i2p/hosts
-wget http://stats.i2p/cgi-bin/newhosts.txt -e use_proxy=yes -e http_proxy=127.0.0.1:4444 -O stats.i2p/hosts || git checkout stats.i2p/hosts
-wget http://no.i2p/export/alive-hosts.txt -e use_proxy=yes -e http_proxy=127.0.0.1:4444 -O no.i2p/hosts || git checkout no.i2p/hosts
+#!/bin/bash
+while IFS='' read -r link || [[ -n "$link" ]]; do
+	HOSTNAME=($(echo $link | awk -F/ '{print $3}'))
+	if [ ! -d "$HOSTNAME" ]; then
+		mkdir $HOSTNAME
+	fi
+    wget $link -e use_proxy=yes -e http_proxy=127.0.0.1:4444 -O "$HOSTNAME"/hosts || 
+    if [ $? -ne 0 ]; then
+		git checkout "$HOSTNAME"/hosts
+    fi
+done < hostslink
 
 M=$(git status --porcelain 2>/dev/null| grep "^ M" | wc -l)
 if [ $M -gt 0 ]
